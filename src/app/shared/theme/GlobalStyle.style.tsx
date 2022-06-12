c4a6a519a57c493706ac1e47a6f660b2e7e0aa1a
felipe <fl.louzeiro@gmail.com>
2022-06-11 23:56:35 -0300

    feat: ajustando estilo global e importando fontes

--- src/app/shared/theme/GlobalStyle.style.tsx
+++ src/app/shared/theme/GlobalStyle.style.tsx
@@ -1,10 +1,17 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 body {
   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
 
-  background-color: #E5E5E5;
-  font-family: 'Montserrat' sans-serif;
+  background-color: #f6f6f6;
+  font-family: 'Montserrat', sans-serif;
+  font-style: normal;
 }
 `
