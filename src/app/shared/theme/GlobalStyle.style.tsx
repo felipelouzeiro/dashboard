e43972aab87ed1bd935cc80c0b3f4737565e1994
felipe <fl.louzeiro@gmail.com>
2022-06-12 00:31:29 -0300

    fix: ajustando importação de fontes para a implantação

--- src/app/shared/theme/GlobalStyle.style.tsx
+++ src/app/shared/theme/GlobalStyle.style.tsx
@@ -1,17 +1,16 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-body {
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
+  body {
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap');
 
-  background-color: #f6f6f6;
-  font-family: 'Montserrat', sans-serif;
-  font-style: normal;
-}
+    background-color: #f6f6f6;
+    font-family: 'Montserrat', sans-serif;
+  }
 `
