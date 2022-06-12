6928c3015783eb23952b13747743aed561e373d9
felipe <fl.louzeiro@gmail.com>
2022-06-11 23:58:39 -0300

    feat: inserindo a Header no componente Dashboard

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,11 @@
 import { GlobalStyle } from '../../shared/theme/GlobalStyle.style'
+import { Header } from './components/header/Header'
 
 export const Dashboard = () => {
   return (
     <>
       <GlobalStyle />
-      header gráficos
+      <Header />
     </>
   )
 }
