1fa2db5ec12eb338c1937b461b80dfb9be2181b4
felipe <fl.louzeiro@gmail.com>
2022-06-11 23:52:15 -0300

    feat: criando a header e componentes estilizados

---
+++ src/app/pages/dashboard/components/header/Header.style.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Zone = styled('header')`
+  background-color: #cec6ce;
+  width: 100%;
+  height: 91px;
+  margin-bottom: 33px;
+`
+export const HeaderContainer = styled('header')`
+  margin: 0 28px 0 28px;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled('h1')`
+  font-size: 28px;
+  line-height: 100%;
+  color: #5d405c;
+  font-weight: 700;
+  width: fit-content;
+`
+
+export const Subtitle = styled('h2')`
+  font-size: 14px;
+  color: #828282;
+  line-height: 100%;
+  font-weight: 500;
+  margin-top: 8px;
+  width: fit-content;
+  letter-spacing: 0.5px;
+`
---
+++ src/app/pages/dashboard/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, Subtitle, Title, Zone } from './Header.style'
+
+export const Header = () => {
+  return (
+    <>
+      <Zone />
+      <HeaderContainer>
+        <Title>Dashboard</Title>
+        <Subtitle>Desafio Técnico Frontend</Subtitle>
+      </HeaderContainer>
+    </>
+  )
+}
