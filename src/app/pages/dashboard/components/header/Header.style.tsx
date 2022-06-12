c9352abea741a914ff9726aa633be9ee028f8df4
felipe <fl.louzeiro@gmail.com>
2022-06-11 23:55:20 -0300

    feat: criando botão filter da header

--- src/app/pages/dashboard/components/header/Header.style.tsx
+++ src/app/pages/dashboard/components/header/Header.style.tsx
@@ -6,10 +6,12 @@ export const Zone = styled('header')`
   height: 91px;
   margin-bottom: 33px;
 `
+
 export const HeaderContainer = styled('header')`
   margin: 0 28px 0 28px;
   align-items: center;
   justify-content: center;
+  position: relative;
 `
 
 export const Title = styled('h1')`
@@ -28,4 +30,26 @@ export const Subtitle = styled('h2')`
   margin-top: 8px;
   width: fit-content;
   letter-spacing: 0.5px;
+`
+
+export const ButtonFilter = styled('button')`
+  position: absolute;
+  right: 0;
+  bottom: 15px;
+  top: 5px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+
+  background-color: #5d405c;
+  width: 100px;
+  height: 40px;
+  border-radius: 10px;
+  border-style: none;
+  color: #f6f6f6;
+  font-weight: 600;
+  line-height: 17px;
+  cursor: pointer;
 `
