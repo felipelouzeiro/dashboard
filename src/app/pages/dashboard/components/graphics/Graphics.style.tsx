b41dc66c87e5e8c95b97b8c9bd827b5de3f52267
felipe <fl.louzeiro@gmail.com>
2022-06-12 23:41:52 -0300

    feat: ajustando responsividade para displays abaixo de 680px

--- src/app/pages/dashboard/components/graphics/Graphics.style.tsx
+++ src/app/pages/dashboard/components/graphics/Graphics.style.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 
+export const device = {
+  mobile: `(max-width: 680px)`,
+}
+
 export const GraphicContainer = styled('section')`
   margin-top: 45px;
   padding: 0 28px;
   display: flex;
-  align-items: center;
+  align-content: center;
   justify-content: center;
   gap: 28px;
   width: 100%;
@@ -19,5 +23,10 @@ export const GraphicContainer = styled('section')`
     height: 100%;
     box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
   }
-  /* max-width: 1440px; */
+
+  @media ${device.mobile} {
+    flex-direction: column;
+    height: 640px;
+  }
 `
+/* max-width: 1440px; */
